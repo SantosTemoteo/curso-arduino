e926a3e8093141f8b3faecc7b67416acf81d5a0d
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-07-30 18:59:46 -0300

    Aula2

--- aula-02/aula-02.ino
+++ aula-02/aula-02.ino
@@ -1,9 +1,11 @@
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(13,OUTPUT); //método que define o estado do pino (módo da porta).   OUTPUT = Saída  - INPUT = Entrada.
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() { // Definir o Estado da porta(HIGH or LOW)
+  digitalWrite(13,HIGH); // HIGH=Ligada(Fornecendo 5v(Saída))   -   LOW=Desligada(0v.   LED ligado ao pino 13 vai acender.
+  delay(2000); // 2000 miliseconds = 2s
+  digitalWrite(13,LOW); // Após 2s por a porta 13 em LOW
+  delay(2000);  // Aguardar mais 2s. e assim fica o código
 }
