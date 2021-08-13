6e7c98a9654da277b1be79d9fff12311e0c3872e
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-13 18:13:50 -0300

    digitalRead e Push Button

--- aula-06/aula-06.ino
+++ aula-06/aula-06.ino
@@ -1,9 +1,36 @@
+#define led 4
+#define btn 3
+int btn_up = 0;
+int btn_dwn = 0;
+
+void troca_led(){ // função troca estado do led
+  if(digitalRead(led) == HIGH){ // lê a porta do led e verifica se ele ta em HIGH
+    digitalWrite(led,LOW);
+  }else{
+    digitalWrite(led,HIGH);
+  }
+}
+
+void key_up(){ // controla o estado do botão
+  if((btn_up == 1) and (btn_dwn == 1)){
+    btn_up = 0;
+    btn_dwn = 0;
+    troca_led();
+  }
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(led,OUTPUT); // definir o modo do led e botão se é de entrada ou saída
+  pinMode(btn,INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (digitalRead(btn) == HIGH){
+    btn_up = 1;
+    btn_dwn = 0;
+    }else{
+      btn_dwn = 1;
+    }
+  key_up();
 }
