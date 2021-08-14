200f6a29f115deb9b38a772661d8a97085b4a3cc
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-13 21:35:06 -0300

    Resistor Pull up/ Pull down

---
+++ aula-07/aula-07.ino
@@ -0,0 +1,27 @@
+
+#define led1 4
+#define btn1 3
+#define led2 7
+#define btn2 6
+
+void setup(){
+  pinMode(led1,OUTPUT);
+  pinMode(btn1,INPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(btn2,INPUT);
+}
+
+void loop() {
+  
+  if(digitalRead(btn1) == HIGH){
+    digitalWrite(led1,HIGH);  
+  }else{
+    digitalWrite(led1,LOW);
+  }
+  
+  if(digitalRead(btn2) == HIGH){
+    digitalWrite(led2,HIGH);  
+  }else{
+    digitalWrite(led2,LOW);
+  }
+}
