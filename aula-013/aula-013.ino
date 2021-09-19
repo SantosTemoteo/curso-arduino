39d049a271186873a58b553bd2e4e9dc19affd3b
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-09-19 00:01:56 -0300

    Sensor LDR

---
+++ aula-013/aula-013.ino
@@ -0,0 +1,32 @@
+//link
+//Sensor LDR, fotoresistor (light dependent resistor)
+
+#define ldr A0
+#define led 2
+
+int vldr=0; // receber um valor de leitura do LDR, escopo global declara-se só 1 vez (não no loop)
+
+void setup() {
+  pinMode(ldr, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);       // monitor serial
+}
+
+void loop() {
+  vldr=analogRead(ldr);
+  if(vldr<900){         // valor de Luminosidade, < ou > muda o sentudo
+    digitalWrite(led,LOW);
+  }else{
+    digitalWrite(led, HIGH);
+  }
+  Serial.println(vldr);
+  delay(100);
+
+}
+
+/*
+ * Com uma resistência variável como o potenciômetro, mas com a variação da resistência pela inscidência de luz sobre ele
+ * por padrão, quando Maior a incidência de luz sobre ele, Menor a reistência.
+ * 
+ * Não tem polaridade, mas se inverte-lo no circuito, inverte seu Funcionamento.
+ */
